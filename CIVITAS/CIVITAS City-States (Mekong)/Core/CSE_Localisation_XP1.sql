650aff6bfa6349d808ee2c46f1cf93a78179b824
Bruno Henrique Rodrigues <41806574+brunohr@users.noreply.github.com>
2020-06-13 13:18:22 -0300

    Update CSE_Localisation_XP1.sql

--- CIVITAS/CIVITAS City-States (Mekong)/Core/CSE_Localisation_XP1.sql
+++ CIVITAS/CIVITAS City-States (Mekong)/Core/CSE_Localisation_XP1.sql
@@ -1,14 +1,58 @@
 /*
-	Localisation XP1
-	Authors: ChimpanG, cachaco
+	Localisation
+	Authors: thecrazyscotsman, ChimpanG, cachaco
 */
 
 INSERT OR REPLACE INTO LocalizedText (Language, Tag, Text)
-VALUES	
+VALUES
 
 -----------------------------------------------
 -- Agrarian
 -----------------------------------------------
 
-	("en_US",	"LOC_CSE_AGRICULTURAL_TRAIT_LARGEST_INFLUENCE_MEKONG_BONUS",	"+3 [ICON_FOOD] Food in every Food Market or Shopping Mall."	),
-	("pt_BR",	"LOC_CSE_AGRICULTURAL_TRAIT_LARGEST_INFLUENCE_MEKONG_BONUS",	"+3 de [ICON_FOOD] Alimento em cada Mercadão ou Shopping."	);
+	("en_US",	"LOC_CSE_AGRICULTURAL_TRAIT_LARGEST_INFLUENCE_BONUS",	"+2 [ICON_FOOD] Food in every Food Market and Shopping Mall."	),
+
+	("pt_BR",	"LOC_CSE_AGRICULTURAL_TRAIT_LARGEST_INFLUENCE_BONUS",	"+2 de [ICON_FOOD] Alimento em cada Mercadão and Shopping."	),
+
+-----------------------------------------------
+-- Consular
+-----------------------------------------------
+
+	("en_US",	"LOC_CITY_STATES_TYPE_CSE_CONSULAR",					"Consular"	),
+	
+	("en_US",	"LOC_CSE_CONSULAR_TRAIT_SMALL_INFLUENCE_BONUS",			"+2 Loyalty Pressure in the [ICON_CAPITAL] Capital."	),
+	("en_US",	"LOC_CSE_CONSULAR_TRAIT_MEDIUM_INFLUENCE_BONUS",		"+2 Loyalty in every City."		),
+	("en_US",	"LOC_CSE_CONSULAR_TRAIT_LARGE_INFLUENCE_BONUS",			"+2 Loyalty Pressure in every City."	),
+	("en_US",	"LOC_CSE_CONSULAR_TRAIT_LARGEST_INFLUENCE_BONUS",		"+2 Loyalty Pressure in every City with an established [ICON_GOVERNOR] Governor."	),
+
+	("en_US",	"LOC_CSE_CONSULAR_TRAIT_NAME",							"{LOC_CITY_STATES_TYPE_CSE_CONSULAR} City-State"	),
+	("en_US",	"LOC_CSE_CONSULAR_TRAIT_DESCRIPTION",					"{LOC_MINOR_CIV_SMALL_INFLUENCE_ENVOYS} {LOC_CSE_CONSULAR_TRAIT_SMALL_INFLUENCE_BONUS}[NEWLINE]{LOC_MINOR_CIV_MEDIUM_INFLUENCE_ENVOYS} {LOC_CSE_CONSULAR_TRAIT_MEDIUM_INFLUENCE_BONUS}[NEWLINE]{LOC_MINOR_CIV_LARGE_INFLUENCE_ENVOYS} {LOC_CSE_CONSULAR_TRAIT_LARGE_INFLUENCE_BONUS}[NEWLINE]{LOC_MINOR_CIV_LARGEST_INFLUENCE_ENVOYS} {LOC_CSE_CONSULAR_TRAIT_LARGEST_INFLUENCE_BONUS}"	),
+
+	("en_US",	"LOC_PEDIA_CITYSTATES_PAGEGROUP_CSE_CONSULAR_NAME",		"{LOC_CITY_STATES_TYPE_CSE_CONSULAR}"	),
+	
+	("pt_BR",	"LOC_CITY_STATES_TYPE_CSE_CONSULAR",					"Consular"	),
+	
+	("pt_BR",	"LOC_CSE_CONSULAR_TRAIT_SMALL_INFLUENCE_BONUS",			"+2 de Pressão de Lealdade na sua [ICON_CAPITAL] Capital."	),
+	("pt_BR",	"LOC_CSE_CONSULAR_TRAIT_MEDIUM_INFLUENCE_BONUS",		"+2 de Pressão de Lealdade em cada cidade."		),
+	("pt_BR",	"LOC_CSE_CONSULAR_TRAIT_LARGE_INFLUENCE_BONUS",			"+2 de Pressão de Lealdade em cada cidade."	),
+	("pt_BR",	"LOC_CSE_CONSULAR_TRAIT_LARGEST_INFLUENCE_BONUS",		"+2 de Pressão de Lealdade em cada cidade com um [ICON_GOVERNOR] Governador estabelecido."	),
+
+	("pt_BR",	"LOC_CSE_CONSULAR_TRAIT_NAME",							"{LOC_CITY_STATES_TYPE_CSE_CONSULAR} Cidade-Estado"	),
+	("pt_BR",	"LOC_CSE_CONSULAR_TRAIT_DESCRIPTION",					"{LOC_MINOR_CIV_SMALL_INFLUENCE_ENVOYS} {LOC_CSE_CONSULAR_TRAIT_SMALL_INFLUENCE_BONUS}[NEWLINE]{LOC_MINOR_CIV_MEDIUM_INFLUENCE_ENVOYS} {LOC_CSE_CONSULAR_TRAIT_MEDIUM_INFLUENCE_BONUS}[NEWLINE]{LOC_MINOR_CIV_LARGE_INFLUENCE_ENVOYS} {LOC_CSE_CONSULAR_TRAIT_LARGE_INFLUENCE_BONUS}[NEWLINE]{LOC_MINOR_CIV_LARGEST_INFLUENCE_ENVOYS} {LOC_CSE_CONSULAR_TRAIT_LARGEST_INFLUENCE_BONUS}"	),
+
+	("pt_BR",	"LOC_PEDIA_CITYSTATES_PAGEGROUP_CSE_CONSULAR_NAME",		"{LOC_CITY_STATES_TYPE_CSE_CONSULAR}"	),
+
+-----------------------------------------------
+-- Entertainment
+-----------------------------------------------
+
+	("en_US",	"LOC_CSE_ENTERTAINMENT_TRAIT_SMALL_INFLUENCE_BONUS",		"+1 [ICON_AMENITIES] Amenity in the [ICON_CAPITAL] Capital."	),
+	("en_US",	"LOC_CSE_ENTERTAINMENT_TRAIT_MEDIUM_INFLUENCE_BONUS",		"+1 [ICON_AMENITIES] Amenity in every Arena or Ferris Wheel."		),
+	("en_US",	"LOC_CSE_ENTERTAINMENT_TRAIT_LARGE_INFLUENCE_BONUS",		"+1 [ICON_AMENITIES] Amenity in every Zoo or Aquarium."	),
+	("en_US",	"LOC_CSE_ENTERTAINMENT_TRAIT_LARGEST_INFLUENCE_BONUS",		"+1 [ICON_AMENITIES] Amenity in every Stadium or Aquatics Center."	);
+	
+	("pt_BR",	"LOC_CSE_ENTERTAINMENT_TRAIT_SMALL_INFLUENCE_BONUS",		"+1 de [ICON_AMENITIES] Serviços na sua [ICON_CAPITAL] Capital."	),
+	("pt_BR",	"LOC_CSE_ENTERTAINMENT_TRAIT_MEDIUM_INFLUENCE_BONUS",		"+1 de [ICON_AMENITIES] Serviços em cada Arena ou Roda Gigante."		),
+	("pt_BR",	"LOC_CSE_ENTERTAINMENT_TRAIT_LARGE_INFLUENCE_BONUS",		"+1 de [ICON_AMENITIES] Serviços em cada Zoológico or Aquário."	),
+	("pt_BR",	"LOC_CSE_ENTERTAINMENT_TRAIT_LARGEST_INFLUENCE_BONUS",		"+1 de [ICON_AMENITIES] Serviços em cada Estádio ou Parque Aquático."	);
+	
